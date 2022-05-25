58eb0d25c7388907933a13ab7381e83c414cd76b
neon_arch <mustafadhuleb53@gmail.com>
2022-05-25 12:34:50 +0300

    update README.org neon-logout.cpp & add sleep.png hybrid.png,etc

--- neon-logout.cpp
+++ neon-logout.cpp
@@ -77,7 +77,7 @@ int main(int argc, char **argv)
   QObject::connect(Btn5, &QPushButton::clicked,[=] () {
     runCommand("slock");
     });
-  Btn5->setIcon(QIcon("/opt/neon-logout/night-mode.png"));
+  Btn5->setIcon(QIcon("/opt/neon-logout/lock.png"));
   Btn5->setIconSize(QSize(100,100));
   Btn5->setStyleSheet("QPushButton{border-radius:50%;}");
   lay->addWidget(Btn5,0,4,1,1);
@@ -100,6 +100,34 @@ int main(int argc, char **argv)
   Label6->setStyleSheet("QLabel{font-weight:600;letter-spacing:1px;font-size:15px;}");
   Label6->setAlignment(Qt::AlignCenter);
   lay->addWidget(Label6,1,5,1,1);
+
+  QPushButton *Btn7 = new QPushButton(&win);
+  QObject::connect(Btn7, &QPushButton::clicked,[=] () {
+    runCommand("systemctl suspend");
+    });
+  Btn7->setIcon(QIcon("/opt/neon-logout/sleep.png"));
+  Btn7->setIconSize(QSize(100,100));
+  Btn7->setStyleSheet("QPushButton{border-radius:50%;}");
+  lay->addWidget(Btn7,0,6,1,1);
+
+  QLabel *Label7 = new QLabel("Sleep",&win);
+  Label7->setStyleSheet("QLabel{font-weight:600;letter-spacing:1px;font-size:15px;}");
+  Label7->setAlignment(Qt::AlignCenter);
+  lay->addWidget(Label7,1,6,1,1);
+
+  QPushButton *Btn8 = new QPushButton(&win);
+  QObject::connect(Btn8, &QPushButton::clicked,[=] () {
+    runCommand("systemctl hybrid-sleep");
+    });
+  Btn8->setIcon(QIcon("/opt/neon-logout/hybrid.png"));
+  Btn8->setIconSize(QSize(100,100));
+  Btn8->setStyleSheet("QPushButton{border-radius:50%;}");
+  lay->addWidget(Btn8,0,7,1,1);
+
+  QLabel *Label8 = new QLabel("Hybrid Sleep",&win);
+  Label8->setStyleSheet("QLabel{font-weight:600;letter-spacing:1px;font-size:15px;}");
+  Label8->setAlignment(Qt::AlignCenter);
+  lay->addWidget(Label8,1,7,1,1);
 
   win.show();
   return app.exec();
